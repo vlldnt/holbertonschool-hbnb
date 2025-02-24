e2ac957db5d645160092ffa960db62ec51d577c9
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-24 11:04:59 +0100

    Add Place class enhancements: generate unique ID, timestamps, and save method

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 '''Place Class'''
 
-from basemodel import BaseModel
+import uuid
+from datetime import datetime
+from .basemodel import BaseModel
+from .user import User
 
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
+        self.id = str(uuid.uuid4())
         self.title = title
         self.description = description
         self.price = price
@@ -14,11 +18,19 @@ class Place(BaseModel):
         self.owner = owner
         self.reviews = []  # List to store related reviews
         self.amenities = []  # List to store related amenities
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def add_review(self, review):
         """Add a review to the place."""
         self.reviews.append(review)
+        self.save()
 
     def add_amenity(self, amenity):
         """Add an amenity to the place."""
         self.amenities.append(amenity)
+        self.save()
+
+    def save(self):
+        """Update the updated_at timestamp whenever the object is modified"""
+        self.updated_at = datetime.now()
