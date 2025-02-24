e7b1c1f99df3b830b6701df7fe5ece0e9573d729
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-24 13:42:04 +0100

    Refactor Review class: move implementation to review.py and delete old file

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,2 +1,15 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 '''Review Class'''
+
+from .base_model import BaseModel
+
+
+class Review(BaseModel):
+    def __init__(self, id, text, user, place, rating, created_at, updated_at):
+        self.id = id
+        self.user = user
+        self.place = place
+        self.text = text
+        self.rating = rating
+        self.created_at = created_at
+        self.updated_at = updated_at
