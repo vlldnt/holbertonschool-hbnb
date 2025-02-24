b18c9365215155ce46a8afe77693708bb0475abf
vlldnt <vieilledent.adrien@gmail.com>
2025-02-24 11:26:33 +0100

    update Face with import of User form app.models.user

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,6 +1,12 @@
+from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
 
     def create_user(self, user_data):
         user = User(**user_data)
