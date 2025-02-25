d8c5f61ca5505aa3e77831633456da44350e284c
vlldnt <vieilledent.adrien@gmail.com>
2025-02-25 10:33:14 +0100

    Pycodestyle for amenity.py

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -6,7 +6,9 @@ from .basemodel import BaseModel
 
 
 class Amenity(BaseModel):
+    '''représents a amenity with attributes and restrictions'''
     def __init__(self, name):
+        '''Initialize a new Amenity instance with restrictions'''
         super().__init__()
         self.name = name
         self.restrictions()
@@ -14,4 +16,4 @@ class Amenity(BaseModel):
     def restrictions(self):
         '''Amenity must exist and must be a string of 50 characters maximum'''
         if len(self.name) > 50 or not self.name:
-            raise ValueError("Amenity must be a string with a maximum of 50 characters.")
+            raise ValueError("Amenity must be a string with max 50 characters")
