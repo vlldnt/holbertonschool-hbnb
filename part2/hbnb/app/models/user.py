6f7f51d7218dabb0a6524cb177694763d5c4f17e
vlldnt <vieilledent.adrien@gmail.com>
2025-02-25 10:27:09 +0100

    adding a raise in user.py for admin

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -3,12 +3,13 @@
 Defines a User class that inherits from BaseModel
 including attributes for user information and restrictions'''
 
-
 import re
 from .basemodel import BaseModel
 
 
 class User(BaseModel):
+    '''Represents a user with various attributes and restrictions'''
+
     def __init__(self, first_name, last_name, email, is_admin=False):
         '''Initialize a new User instance with validation'''
         super().__init__()
@@ -19,20 +20,20 @@ class User(BaseModel):
         self.restrictions()
 
     def restrictions(self):
-        '''Validate the user's attributes for length and format restrictions'''
-        
+        '''Validate the user's attributes for len and format restrictions'''
         if len(self.first_name) > 50 or not self.first_name:
-            raise ValueError("The maximum first name length is 50 characters.")
+            raise ValueError("The maximum first name length is 50 characters")
         if not isinstance(self.first_name, str):
             raise TypeError("First name must be a string of characters.")
-        
+
         if len(self.last_name) > 50 or not self.last_name:
             raise ValueError("The maximum last name length is 50 characters.")
         if not isinstance(self.last_name, str):
-            raise TypeError("Last name must be a string of characters")
-        
-        if not self.email or not re.fullmatch(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', self.email):
+            raise TypeError("Last name must be a string of characters.")
+
+        if not self.email or not re.fullmatch(
+            r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', self.email):
             raise ValueError("The email format is invalid.")
-        
+
         if not isinstance(self.is_admin, bool):
-            TypeError("Admin must be a boolean")
+            raise TypeError("Admin must be a boolean.")
