483c05f6e095284887216336b330fa93942f23fe
vlldnt <vieilledent.adrien@gmail.com>
2025-02-25 11:13:16 +0100

    correction code on user.py

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -31,8 +31,10 @@ class User(BaseModel):
             raise ValueError("The maximum last name length is 50 characters.")
         if not isinstance(self.last_name, str):
             raise TypeError("Last name must be a string of characters")
-        
-        if not re.fullmatch(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', self.email):
+
+        if not re.fullmatch(
+            r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', self.email
+        ):
             raise ValueError("The email format is invalid.")
 
         if not isinstance(self.is_admin, bool):
