4504f1dc8ee13191f936d6dc8ee1076053bb1b03
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-27 20:06:47 +0100

    Update first and last name validation to allow only letters and spaces

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -25,9 +25,9 @@ class User(BaseModel):
 
     @first_name.setter
     def first_name(self, value):
-        if len(value) > 50 or value.startswith(' '):
+        if len(value) > 50 or not re.fullmatch(r'^[A-Za-z ]+$', value):
             raise ValueError(
-                "First name must be present with a maximum of 50 characters and cannot start with a space."
+                "First name must be present with a maximum of 50 characters and can only contain letters and spaces."
             )
         if not isinstance(value, str):
             raise TypeError("First name must be a string of characters.")
@@ -39,9 +39,9 @@ class User(BaseModel):
 
     @last_name.setter
     def last_name(self, value):
-        if len(value) > 50 or value.startswith(' '):
+        if len(value) > 50 or not re.fullmatch(r'^[A-Za-z ]+$', value):
             raise ValueError(
-                "Last name must be present with a maximum of 50 characters and cannot start with a space."
+                "Last name must be present with a maximum of 50 characters and can only contain letters and spaces."
             )
         if not isinstance(value, str):
             raise TypeError("Last name must be a string of characters.")
