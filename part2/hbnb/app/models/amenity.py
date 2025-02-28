80376d7fc4e4c86d76197984b88c4cbf6907046c
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-02-28 12:02:12 +0100

    Refactor Amenity class: streamline initialization and validation for name attribute

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -6,19 +6,11 @@ from .basemodel import BaseModel
 
 
 class Amenity(BaseModel):
-    '''représents a amenity with attributes and restrictions'''
+    '''Represents an amenity with attributes and restrictions'''
     def __init__(self, name):
         '''Initialize a new Amenity instance with restrictions'''
         super().__init__()
-        self._name = name
-        self.restrictions()
-
-    def restrictions(self):
-        '''Amenity must exist and must be a string of 50 characters maximum'''
-        if len(self._name) > 50 or not self.name:
-            raise ValueError("Amenity must be a string with max 50 characters")
-        if not isinstance(self._name, str):
-            raise TypeError("Name must be a string of characters")
+        self.name = name
 
     @property
     def name(self):
@@ -27,9 +19,7 @@ class Amenity(BaseModel):
     @name.setter
     def name(self, value):
         if len(value) > 50 or not value:
-            raise ValueError(
-                "Amenity name must be a string with max 50 characters"
-            )
+            raise ValueError("Amenity name must be a string with max 50 characters")
         if not isinstance(value, str):
             raise TypeError("Amenity name must be a string of characters.")
         self._name = value
