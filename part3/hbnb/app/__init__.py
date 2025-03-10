53d6256d4de6ef47cf8e440995b0949283482623
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-03-10 17:01:37 +0100

    Add JWT authentication namespace and initialize JWT manager

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,11 +2,14 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -19,7 +22,9 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     return app
