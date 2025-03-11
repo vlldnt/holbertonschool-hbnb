b44bb76e112b75b0ad82c189a4aad2a24ec2938b
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-03-11 13:17:37 +0100

    Add JWTManager import and initialize auth namespace in app

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -15,6 +17,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API')
     
+    from app.api.v1.auth import api as auth_ns
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
