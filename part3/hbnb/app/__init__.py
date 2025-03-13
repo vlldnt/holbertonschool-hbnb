3acba8a82d41421df2ee81ffba21f6457452eba3
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2025-03-13 09:47:04 +0100

    Add auth namespace to API and reorder existing namespaces

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -23,11 +23,11 @@ def create_app(config_class="config.DevelopmentConfig"):
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
     
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     bcrypt.init_app(app)
     jwt.init_app(app)
