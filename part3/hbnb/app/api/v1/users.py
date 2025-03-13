844ebb9de9dd4733374bcbc6466e8ae0cb9b55d5
vlldnt <vieilledent.adrien@gmail.com>
2025-03-13 10:06:58 +0100

    Remove password field from user response in UserList resource

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -50,7 +50,6 @@ class UserList(Resource):
                 'first_name': user.first_name,
                 'last_name': user.last_name,
                 'email': user.email,
-                'password': user.password
             }
             for user in users
         ], 200
