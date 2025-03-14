906e7744d6a750bf6c89b2d90a0a28a51b94d1a7
vlldnt <vieilledent.adrien@gmail.com>
2025-03-14 15:19:26 +0100

    Refactor database schema: consolidate SQL scripts and recreate users, places, reviews, amenities, and place_amenities tables

--- part3/hbnb/SQL_scripts/tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id CHAR(36) PRIMARY KEY,
-    first_name VARCHAR(255)
-    last_name VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255),
-    is_admin BOOLEAN DEFAULT FALSE
-);
