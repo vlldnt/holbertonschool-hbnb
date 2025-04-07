a57b810e46fdc4610f3004fdfa7fd814886d5b5b
vlldnt <vieilledent.adrien@gmail.com>
2025-04-07 16:06:46 +0200

    adding dynamic button

--- part4/assets/js/scripts.js
+++ part4/assets/js/scripts.js
@@ -155,4 +155,10 @@ function displayDetailedPlaces(place) {
     } else {
         reviewsPlace.innerHTML += '<p>No reviews available for this place.</p>';
     }
-}
+
+    const addReviewContainer = document.getElementById('add-review');
+    const reviewLink = document.createElement('a');
+    reviewLink.href = `add_review.html?id=${place.id}`;
+    reviewLink.innerHTML = `<button>Add a review</button>`;
+    addReviewContainer.appendChild(reviewLink);
+};
